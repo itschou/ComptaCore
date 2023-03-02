513b9fdbdca20ac96ad49d580fcee4a90268a799
Cat4clysme <ouwoxpro@gmail.com>
2023-03-02 14:27:52 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"/>
+
+        <title>Demande Devis </title>
+
+    </head>
+    <body>
+
+        <div class="container">
+            <h1 class="text-center text-uppercase">Demander un devis</h1>
+            <select name=""></select>
+        </div>
+        
+    </body>
+</html>
